69dfe7cf7bfe9d2c0ed311081923327f451cd467
AnishkaR10 <anishkaraghuwanshi2005@gmail.com>
2025-06-14 22:21:11 +0530

    Update Layout.tsx

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      <main className="flex-grow mt-20">{children}</main>
+      <main className="flex-grow mt-0">{children}</main>
       
     </div>
   );
